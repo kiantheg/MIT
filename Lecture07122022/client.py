726dc42062fb3e7300ecd7156af3b9b2225ab181
Alice Xiao <alice.xiaoziqian@gmail.com>
2022-07-12 16:07:57 -0400

    Create client.py

---
+++ Lecture07122022/client.py
@@ -0,0 +1,23 @@
+import socket
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = 10000
+#s.connect(('localhost', port))
+server_address = ('localhost', port)
+s.connect(server_address)
+z = 'Hello World'
+s.sendall(z.encode())
+
+data, address = s.recvfrom(4096)
+data = data.decode('utf-8')
+logger.info(data)
+s.close()
