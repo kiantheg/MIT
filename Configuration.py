d629572fad6cf20e7807f346e4ca4dff988e4799
rishitadhalbisoi <79031603+rishitadhalbisoi@users.noreply.github.com>
2022-07-25 12:23:56 -0700

    Update Configuration.py

--- Configuration.py
+++ Configuration.py
@@ -1,4 +1,6 @@
 from Point import Point
+import glob
+import os
 
 #Constants
 SPEED_OF_LIGHT = 299792458 # (m/s)
@@ -9,15 +11,16 @@ CENTERED_FREQUENCY = 4.3 * 10e9
 CENTERED_WAVELENGTH = SPEED_OF_LIGHT/CENTERED_FREQUENCY
 VELOCITY = 66.730296
 
+list_of_files = glob.glob('/Users/rishita/bwsi22/emulator/output/*') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getctime)
+
 CPI = 0.74
-PLATFORM_POS = "/Users/kianchen/Desktop/BeaverWorks/emulator/output/20220725T115842_5_point_scatter_platform_pos.pkl"
+PLATFORM_POS = latest_file
 #RANGE_RESOLUTION = SPEED_OF_LIGHT/(2* BANDWIDTH)
 #CROSS_RANGE_RESOLUTION = CENTERED_WAVELENGTH * RANGE_TO_TARGET / (2*VELOCITY*CPI)
-COORDINATES = [-10,10,-10,10] # x_start, x_end, y_start, y_end
 RANGE_RESOLUTION = 0.1
 CROSS_RANGE_RESOLUTION = 0.1
 
-SCAN_COUNT = 1000
 SCAN_START = 0 #+/-499,998 ps
 SCAN_END = int(2*26e12/SPEED_OF_LIGHT) #+/-499,998 ps
 SCAN_RES = 32 #1-511
