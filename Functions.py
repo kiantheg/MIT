cceed05e79bd2f8783a865ec6ad64d77341f4b29
Alice Xiao <alice.xiaoziqian@gmail.com>
2022-07-25 14:11:06 -0400

    changes

---
+++ Functions.py
@@ -0,0 +1,18 @@
+import math
+from matplotlib import pyplot as plt
+import numpy as np
+from Configuration import SPEED_OF_LIGHT
+from Point import Point
+
+
+##R = tc/2
+
+def tou(a, b):
+    return 2*(a.distance(b))/SPEED_OF_LIGHT
+#grid = np.zeros((int(LENGTH), int(WIDTH)))
+#plt.imshow(grid, cmap='gray')
+
+#print(RESOLUTION)
+#print(CROSS_RESOLUTION)
+
+#RCS = TOU
