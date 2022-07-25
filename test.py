e2e3b1f5446f1705f2689807ea9f03523a2b4752
Alice Xiao <alice.xiaoziqian@gmail.com>
2022-07-25 15:31:13 -0400

    blablabla

--- test.py
+++ test.py
@@ -10,13 +10,13 @@ import time
             time.sleep(.001)
             bar()'''
 # load : get the data from file
-#data = pickle.load(open("/Users/zxiao23/Desktop/BWSISummer/team5/emulatorTest/output/PLATFORMPOS.pkl", "rb"))
-#print(data)
+data = pickle.load(open("/Users/zxiao23/Desktop/BWSISummer/team5/mypickle.pickle", "rb"))
+print(data)
 
 #print((20 - 1.14627368)/2000/200000E-12)
 #print((17.16877474 + 20)/2000/400000E-12)
 
 xPos = np.arange(-10,10,0.1)
 yPos = np.arange(-10,10,0.1)
-print(xPos)
-print(yPos)
+#with open('mypickle.pickle', 'wb') as f:
+    #pickle.dump(xPos, f)
