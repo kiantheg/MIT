5d8190cfaf199b9df8697f3cf96ab9ccd29c570c
Alice Xiao <alice.xiaoziqian@gmail.com>
2022-08-01 10:32:04 -0400

    update BPRANGEBINS

--- BPRangeBin.py
+++ BPRangeBin.py
@@ -2,15 +2,15 @@ import pickle as pkl
 import numpy as np
 import matplotlib.pyplot as plt
 from alive_progress import alive_bar
-from Configuration import SCAN_COUNT, SPEED_OF_LIGHT, RANGE_RESOLUTION, CROSS_RANGE_RESOLUTION, PLATFORM_POS, COORDINATES, SCAN_RES
+from Configuration import COORDINATES, SPEED_OF_LIGHT, CROSS_RANGE_RESOLUTION, RANGE_RESOLUTION
 
-#read datalist from pickle file
-datalist = pkl.load(open("/Users/zxiao23/Desktop/BWSISummer/team5/datalist.pkl", "rb"))
+#change this to local file
+data = pkl.load(open("/Users/zxiao23/Desktop/BWSISummer/emulator/input/marathon_0.pkl", "rb"))
 
-def readPlatformPos(filepath):
-    data = pkl.load(open(filepath, "rb"))
-    platformPos = data['platform_pos']
-    return platformPos
+datalist = data['scan_data']
+platformPos = data['platform_pos']
+rangeBins = data['range_bins']
+SCAN_COUNT = len(data['scan_data'])
 
 def paintImage(datalist, rangeBins, platformPos, xCor, yCor, zOffset = 0):
     numX = len(xCor)
@@ -22,7 +22,7 @@ def paintImage(datalist, rangeBins, platformPos, xCor, yCor, zOffset = 0):
             yNP = np.asarray((yCor[:] - platformPos[scan][1])**2)
             temp = np.zeros((numX, numY))
             temp = xNP[np.newaxis,:] + yNP[:, np.newaxis]
-            temp = np.sqrt(temp+(zOffset - platformPos[scan][2])**2) * 2e12 / SPEED_OF_LIGHT / (SCAN_RES*1.907)
+            #needs to change temp = np.sqrt(temp+(zOffset - platformPos[scan][2])**2) * 2e12 / SPEED_OF_LIGHT / (SCAN_RES*1.907)
             #closestIndex = np.array(2*np.sqrt((xCor[:] - platformPos[scan][0])**2 + (yCor[:] - platformPos[scan][1])**2 + (zOffset - platformPos[scan][2])**2) * 1e12 / SPEED_OF_LIGHT / 61)
             image[:] += datalist[scan][np.minimum(temp, np.full((numX,numY),len(datalist[0])-1)).astype(int)]
 
@@ -34,7 +34,7 @@ def paintImage(datalist, rangeBins, platformPos, xCor, yCor, zOffset = 0):
 xPos = np.arange(COORDINATES[0],COORDINATES[1],CROSS_RANGE_RESOLUTION)
 yPos = np.arange(COORDINATES[2],COORDINATES[3],RANGE_RESOLUTION)
 
-plt.imshow(paintImage(datalist, readPlatformPos(PLATFORM_POS), xPos, yPos), cmap='gray', origin='lower', extent=COORDINATES)
+plt.imshow(paintImage(datalist, rangeBins, platformPos, xPos, yPos), cmap='gray', origin='lower', extent=COORDINATES)
 plt.colorbar()
 plt.xlabel("x-axis (meters/"+str((COORDINATES[1]-COORDINATES[0])/RANGE_RESOLUTION)+" pixels)")
 plt.ylabel("y-axis (meters/"+str((COORDINATES[3]-COORDINATES[2])/CROSS_RANGE_RESOLUTION)+" pixels)")
