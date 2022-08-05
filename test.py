f80af3a756a63503f97c4eff46403cea0832fb8e
Alice <xiaoziqian.alice@gmail.com>
2022-08-05 11:26:05 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 import pickle              # import module first
 
@@ -6,3 +7,14 @@ mydict = pickle.load(f)         # load file content as mydict
 f.close()                       
 
 print(mydict)
+=======
+import pickle as pkl
+import glob
+import os
+from Configuration import COORDINATES
+import numpy as np
+import matplotlib.pyplot as plt
+img = pkl.load(open("hide_and_seek_8_img.pkl", "rb"))
+plt.imshow(img['img'], cmap='gray', origin='lower', extent=COORDINATES)
+plt.show()
+>>>>>>> Stashed changes
